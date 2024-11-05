c54148b52ade0eae4110c836601ec4b14edd13eb
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-11-05 10:44:13 +0200

    simplify color theme options

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,59 +1,57 @@
 return {
 
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	opts = {
-	-- 		transparent = true,
-	-- 		on_colors = function(colors)
-	-- 			colors.error = "#ff0000"
-	-- 			-- colors.bg = "#000000"
-	-- 			colors.warning = "#ff8800"
-	-- 		end,
-	-- 	},
-	-- 	config = function(_, opts)
-	-- 		require("tokyonight").setup(opts)
-	-- 		vim.cmd("colorscheme tokyonight")
-	-- 	end,
-	-- },
-	{
-		--rose-pine
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		opts = {
-	           styles = {
-	               transparency = true,
-	           },
-	           groups = {
-	               error ="#ff0000"
-	           },
-	       },
-		config = function(_, opts)
-	           require("rose-pine").setup(opts)
-	           vim.cmd("colorscheme rose-pine")
-
-	       end,
-	},
+    -- {
+    -- 	"aktersnurra/no-clown-fiesta.nvim",
+    -- 	lazy = false,
+    -- 	priority = 1000,
+    -- 	opts = {
+    -- 		transparent = true,
+    -- 	},
+    -- 	config = function(_, opts)
+    -- 		require("no-clown-fiesta").setup(opts)
+    -- 		vim.cmd("colorscheme no-clown-fiesta")
+    -- 	end,
+    -- },
     {
-		"jnurmine/Zenburn",
-	},
-	{
-		"RRethy/base16-nvim",
-	},
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	opts = {
- --            transparent_background = true,
- --        },
-	-- 	config = function(_, opts)
-	-- 		require("catppuccin").setup(opts)
-	-- 		vim.cmd("colorscheme catppuccin")
-	-- 	end,
-	-- },
+        "AlexvZyl/nordic.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = {
+                bg = true,
+                float = true
+            },
+        },
+        config = function(_, opts)
+            require("nordic").setup(opts)
+            vim.cmd("colorscheme nordic")
+        end,
+    },
+
+
+    -- {
+    --     --rose-pine
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {
+    --         styles = {
+    --             transparency = true,
+    --         },
+    --         groups = {
+    --             error = "#ff0000"
+    --         },
+    --     },
+    --     config = function(_, opts)
+    --         require("rose-pine").setup(opts)
+    --         vim.cmd("colorscheme rose-pine")
+    --     end,
+    -- },
+    {
+        "jnurmine/Zenburn",
+    },
+    {
+        "RRethy/base16-nvim",
+    },
 }
